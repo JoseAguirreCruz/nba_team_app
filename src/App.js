d80b00dff47f68c3543e22be90f485d9a3925131
Jose Aguirre <aguirrejose615@gmail.com>
2023-02-01 21:54:22 -0500

    testing data

--- src/App.js
+++ src/App.js
@@ -8,19 +8,26 @@ import Header from './Components/Header'
 const teams = Object.values(nbateams)
 
 const App = () => {
+  const test1 = teams.map((team) => {
+    return team
+  })
+  const test2 = test1.map((test) => {
+    return test.teamName
+  })
+  console.log(test2)
   return (
     <div>
       <Header />
       <Search />
-      {teams.map((teams, index) => (
+      {teams.map((team, index) => (
         <PlayerTable
           key={index}
-          teamName={teams.teamName}
-          arena={teams.arena}
-          location={teams.location}
-          conference={teams.conference}
-          division={teams.division}
-          players={teams.players}
+          teamName={team.teamName}
+          arena={team.arena}
+          location={team.location}
+          conference={team.conference}
+          division={team.division}
+          players={team.players}
         />
       ))}
     </div>
